983ac8e0f4e9e9495b1681905cb03665d02c2cb2
Vincent <linguofu303@gmail.com>
2020-12-04 23:21:33 +0000

    pipeline

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,8 +3,6 @@
 env DB_URI=${DB_URI}
 env KEY=${KEY}
 docker login -u $dockeru -p $dockerp
+
 sudo docker-compose build
 sudo docker-compose push
-echo "========================"
-echo ${DB_URI}
-echo "========================"
