510240d216e4b117ae7c4dfe51fddf21b73bbadf
Vincent <linguofu303@gmail.com>
2020-12-04 15:48:17 +0000

    Pipeline update

--- scripts/ansible.sh
+++ scripts/ansible.sh
@@ -1,8 +1,6 @@
 #! /bin/bash
-sudo apt update
-sudo apt install ansible -y
-ansible --version
-
-cd ansible
-
+mkdir -p ~/.local/bin
+echo 'PATH=$PATH:~/.local/bin' >> ~/.bashrc
+source ~/.bashrc
+pip3 install --user ansible
 ansible-playbook -i inventory playbook.yaml
