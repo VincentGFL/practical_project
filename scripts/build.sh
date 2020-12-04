430bc66106104cfb9ca0d658d9b6bec1eb226923
Vincent <linguofu303@gmail.com>
2020-12-04 21:34:05 +0000

    pipeline

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,17 +1,5 @@
 #! /bin/bash
 
-cd frontend
-docker build -t vlin303/frontend:latest .
-docker push vlin303/frontend:latest
 
-cd ../classgen
-docker build -t vlin303/classgen:latest .
-docker push vlin303/classgen:latest
-
-cd ../namegen
-docker build -t vlin303/namegen:latest .
-docker push vlin303/namegen:latest
-
-cd ../healthgen
-docker build -t vlin303/healthgen:latest .
-docker push vlin303/healthgen:latest
+sudo docker-compose build
+sudo docker-compose build
