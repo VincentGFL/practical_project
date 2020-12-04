0124a3d798a2dbd43002cceaed7d7c2214d40e0d
Vincent <linguofu303@gmail.com>
2020-12-04 16:38:00 +0000

    Pipeline

--- scripts/ansible.sh
+++ scripts/ansible.sh
@@ -1,6 +1,5 @@
 #! /bin/bash
-mkdir -p ~/.local/bin
-echo 'PATH=$PATH:~/.local/bin' >> ~/.bashrc
+
 source ~/.bashrc
 pip3 install --user ansible
-/home/jenkins/.local/bin/ansible-playbook -v -i inventory practical_projects/ansible/playbook.yaml
+ansible-playbook -v -i inventory practical_projects/ansible/playbook.yaml
