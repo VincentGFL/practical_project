5ec4db66946a41e193ac5f15193914255a7605ad
Vincent <linguofu303@gmail.com>
2020-12-05 23:09:02 +0000

    script update

--- Jenkinsfile
+++ Jenkinsfile
@@ -7,7 +7,6 @@ pipeline{
         stages{
         stage('Ansible'){
             steps{
-                sh "chmod +x -R ${env.WORKSPACE}"
                 sh "./scripts/ansible.sh"
             }
         }
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,7 +5,6 @@ export DB_URI=$JDB_URI
 export KEY=$KEY2
 git clone https://github.com/VincentGFL/practical_project.git
 cd practical_project
-docker stack rm generatorstack
 env DB_URI=$JDB_URI env KEY=$KEY2 docker stack deploy --compose-file docker-compose.yaml generatorstack
 
 EOF
