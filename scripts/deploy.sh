15f0e0a99f1ca7a29b4aa58a0e2b5e9612e822ad
Vincent <linguofu303@gmail.com>
2020-12-05 13:41:31 +0000

    Script update

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,6 +5,7 @@ ssh jenkins@manager << EOF
 git clone https://github.com/VincentGFL/practical_project.git
 cd practical_project
 export DB_URI=${DB_URI}
+echo ${DB_URI}
 export KEY=${KEY}
 docker stack deploy --compose-file docker-compose.yaml generatorstack
 
