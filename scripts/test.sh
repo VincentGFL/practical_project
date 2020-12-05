2835fac968d38b30e4e048dc0486a29f4ca4580d
Vincent <linguofu303@gmail.com>
2020-12-05 15:48:58 +0000

    Script update

--- scripts/test.sh
+++ scripts/test.sh
@@ -3,8 +3,8 @@ cd practical_project
 docker build -t testing-image -f testing/Dockerfile .
 docker run -it -d --name testing-container testing-image
 
-docker exec testing-container export DB_URI='${DB_URI}'
-docker exec testing-container export KEY='${KEY}'
+docker exec testing-container export DB_URI=${DB_URI}
+docker exec testing-container export KEY=${KEY}
 
 docker exec testing-container pytest ./frontend --cov ./application
 docker exec testing-container pytest ./classgen --cov ./application
