95b4b18031e4e4d5994e16f6e1db987a99bdb03e
Vincent <linguofu303@gmail.com>
2020-12-05 15:44:41 +0000

    Script update

--- scripts/test.sh
+++ scripts/test.sh
@@ -6,10 +6,10 @@ docker run -it -d --name testing-container testing-image
 docker exec testing-container export DB_URI='${DB_URI}'
 docker exec testing-container export KEY='${KEY}'
 
-docker exec testing-container env DB_URI='$DB_URI' env KEY='$KEY' pytest ./frontend --cov ./application
-docker exec testing-container env DB_URI='$DB_URI' env KEY='$KEY' pytest ./classgen --cov ./application
-docker exec testing-container env DB_URI='$DB_URI' env KEY='$KEY' pytest ./namegen --cov ./application
-docker exec testing-container env DB_URI='$DB_URI' env KEY='$KEY' pytest ./healthgen --cov ./application
+docker exec testing-container pytest ./frontend --cov ./application
+docker exec testing-container pytest ./classgen --cov ./application
+docker exec testing-container pytest ./namegen --cov ./application
+docker exec testing-container pytest ./healthgen --cov ./application
 
 docker stop testing-container
 docker rm testing-container
