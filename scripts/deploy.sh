17d25c3b544e89d4167a976b501d5ddbc420b62a
Vincent <linguofu303@gmail.com>
2020-12-05 17:13:57 +0000

    script update

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,6 +6,6 @@ export DB_URI='${DB_URI}'
 export KEY='${KEY}'
 git clone https://github.com/VincentGFL/practical_project.git
 cd practical_project
-env DB_URI='$DB_URI' env KEY='$KEY' docker stack deploy --compose-file docker-compose.yaml generatorstack
+env DB_URI=${DB_URI} env KEY=${KEY} docker stack deploy --compose-file docker-compose.yaml generatorstack
 
 EOF
