45fbcd6f213b6c6bcfa6ec90cfaea099929e7b9e
Vincent <linguofu303@gmail.com>
2020-12-05 00:55:56 +0000

    pipeline

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,6 +3,7 @@
 ssh jenkins@manager << EOF
 
 git clone https://github.com/VincentGFL/practical_project.git
+cd practical_project
 docker stack deploy --compose-file docker-compose.yaml generatorstack
 
 EOF
