0899e1cca3e6862bc73c6ac99503253a706600f7
Vincent <linguofu303@gmail.com>
2020-12-06 21:13:17 +0000

    Attempt to fix DB_URI

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,5 +4,5 @@
 #env DB_URI=${DB_URI} env KEY=${KEY} 
 #export DB_URI=${DB_URI}
 #export KEY=${KEY}
-docker-compose down --rmi all
-docker-compose build 
+sudo docker-compose down --rmi all
+sudo docker-compose build 
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,6 +3,8 @@
 #export DB_URI=${DB_URI}
 #export KEY=${KEY}
 ssh jenkins@manager << EOF
+git clone https://github.com/VincentGFL/practical_project.git
+cd practical_project
 
 docker stack rm generatorstack 
 docker pull vlin303/frontendimage:latest
@@ -10,8 +12,6 @@ docker pull vlin303/classgenimage:latest
 docker pull vlin303/namegenimage:latest
 docker pull vlin303/healthgen:latest
 
-git clone https://github.com/VincentGFL/practical_project.git
-cd practical_project
 docker stack deploy --compose-file docker-compose.yaml generatorstack
 EOF
 
