8d1a189219b7e7c1c9b045dbfb50c9c12c616d64
Vincent <linguofu303@gmail.com>
2020-12-06 22:22:55 +0000

    Attempt to fix

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -13,6 +13,6 @@ docker pull vlin303/classgenimage:latest
 docker pull vlin303/namegenimage:latest
 docker pull vlin303/healthgen:latest
 
-docker stack deploy --compose-file docker-compose.yaml generatorstack
+env DB_URI=${DB_URI} env KEY=${KEY} docker stack deploy --compose-file docker-compose.yaml generatorstack
 EOF
 
