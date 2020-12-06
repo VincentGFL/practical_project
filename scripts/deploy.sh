1b2cd3aa98ff03dc3c94d18552ea7014f1a35457
Vincent <linguofu303@gmail.com>
2020-12-06 17:22:14 +0000

    Attempt to fix URI

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,7 +7,7 @@ ssh jenkins@manager << EOF
 
 git clone https://github.com/VincentGFL/practical_project.git
 cd practical_project
-docker stack deploy --compose-file docker-compose.yaml generatorstack
+docker stack deploy -e DB_URI=${DB_URI} --compose-file docker-compose.yaml generatorstack
 export DB_URI=${DB_URI}
 export KEY=${KEY}
 exit
