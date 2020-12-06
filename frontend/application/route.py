8bf31a7892f30713203ac0ef5074013783eeb61f
Vincent <linguofu303@gmail.com>
2020-12-06 22:19:25 +0000

    Attempt to fix

--- frontend/application/route.py
+++ frontend/application/route.py
@@ -4,10 +4,10 @@ import requests
 
 @app.route('/', methods=['POST','GET'])
 def index():
-    classes = requests.get("http://34.105.167.64:5001/classgen")
-    names = requests.get("http://34.105.167.64:5002/namegen")
+    classes = requests.get("http://classgen:5001/classgen")
+    names = requests.get("http://namegen:5002/namegen")
 
     character = str(classes.text) + " " + str(names.text)
-    health = requests.post('http://34.105.167.64:5003/healthgen', data=character)
+    health = requests.post('http://healthgen:5003/healthgen', data=character)
 
     return render_template('home.html', character=character, health=health.text)
