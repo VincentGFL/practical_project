882b0e66a5bec405c3479d567a169297f0572fdd
Vincent <linguofu303@gmail.com>
2020-12-06 12:07:39 +0000

    Attempt to fix URI

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,7 +2,7 @@
 
 
 #env DB_URI=${DB_URI} env KEY=${KEY} 
-export DB_URI=${DB_URI}
-export KEY=${KEY}
+#export DB_URI=${DB_URI}
+#export KEY=${KEY}
 #sudo docker-compose down --rmi all
-env DB_URI=${DB_URI} env KEY=${KEY} docker-compose build 
+docker-compose build 
