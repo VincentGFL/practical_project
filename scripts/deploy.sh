03eff502ea9ba685b58cea18ea60f943e628351a
Vincent <linguofu303@gmail.com>
2020-12-06 22:40:01 +0000

    Added new docker-compose

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,11 +7,12 @@ git clone https://github.com/VincentGFL/practical_project.git
 git pull
 cd practical_project
 
-#docker stack rm generatorstack 
+docker stack rm generatorstack 
 docker pull vlin303/frontendimage:latest
 docker pull vlin303/classgenimage:latest
 docker pull vlin303/namegenimage:latest
 docker pull vlin303/healthgen:latest
+docker pull nginx:alpine
 
 env DB_URI=${DB_URI} env KEY=${KEY} docker stack deploy --compose-file docker-compose.yaml generatorstack
 EOF
