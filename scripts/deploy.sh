5ad2c792bd998062acf8d9fdfeb136e35adbdf92
Vincent <linguofu303@gmail.com>
2020-12-07 01:31:15 +0000

    Fixing DB_URI

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,8 +4,7 @@ ssh jenkins@manager << EOF
 git clone https://github.com/VincentGFL/practical_project.git
 cd practical_project
 git pull
-export DB_URI=${DB_URI}
-export KEY=${KEY}
+echo ${DB_URI}
 
 
 env DB_URI=${DB_URI} env KEY=${KEY} docker stack deploy --compose-file docker-compose.yaml generatorstack
