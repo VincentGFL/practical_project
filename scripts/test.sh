6a033f86cb0bf5c435d0706439882cdb59bbe9e6
Vincent <linguofu303@gmail.com>
2020-12-07 00:43:16 +0000

    Fixing DB_URI

--- scripts/test.sh
+++ scripts/test.sh
@@ -4,10 +4,10 @@ cd practical_project
 docker build -t testing-image -f testing/Dockerfile .
 docker run -it -d --name testing-container testing-image
 
-docker exec testing-container pytest ./frontend --cov ./application
-docker exec testing-container pytest ./classgen --cov ./application
-docker exec testing-container pytest ./namegen --cov ./application
-docker exec testing-container pytest ./healthgen --cov ./application
+docker exec testing-container pytest ./frontend --cov frontend/application
+docker exec testing-container pytest ./classgen --cov classgen/application
+docker exec testing-container pytest ./namegen --cov namegen/application
+docker exec testing-container pytest ./healthgen --cov healthgen/application
 
 docker stop testing-container
 docker rm testing-container
