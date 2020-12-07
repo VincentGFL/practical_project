a824a348a1052c12a87c50a17925b1ce77308d29
Vincent <linguofu303@gmail.com>
2020-12-07 01:43:03 +0000

    Fixing DB_URI

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,13 +5,6 @@ git clone https://github.com/VincentGFL/practical_project.git
 cd practical_project
 git pull
 
-
-docker stack rm generatorstack 
-docker pull vlin303/frontendimage:latest
-docker pull vlin303/classgenimage:latest
-docker pull vlin303/namegenimage:latest
-docker pull vlin303/healthgen:latest
-
 env DB_URI=${DB_URI} env KEY=${KEY} docker stack deploy --compose-file docker-compose.yaml generatorstack
 EOF
 
